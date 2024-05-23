25c18ca1d13a0f95d292620448011b02220f3ad5
xamotex1000 <140911479+xamotex1000@users.noreply.github.com>
2024-05-23 12:18:15 -0500

    Adapting To HTML

--- Scripts/Get Data Files.js
+++ Scripts/Get Data Files.js
@@ -1,9 +1,11 @@
 function readCookies() {
-    let cookieArr = document.cookie.split(";");
+    let cookieArr = document.cookie;
     return cookieArr;
 }
-const jsonData = readCookies();
-var string = document.createElement('h1');
-string.appendChild("Works atleast", jsonData)
-document.getElementsByTagName('body')[0].appendChild(string);
+const jsonData = decodeURIComponent(readCookies()).split(';')
+for(var i = 0; i < jsonData.length; i++){
+    jsonData[i] = jsonData[i].split('=')[1].trim();
+}
+jsonData[0] = JSON.parse(jsonData[0])
+jsonData = jsonData[0];
 module.exports = jsonData;
