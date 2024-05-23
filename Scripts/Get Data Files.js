8f7938a41d0bc2c8b0942d480bece58923391b78
xamotex1000 <140911479+xamotex1000@users.noreply.github.com>
2024-05-23 16:10:56 -0500

    aaaaaAAAAFSDAGFAsgfd

--- Scripts/Get Data Files.js
+++ Scripts/Get Data Files.js
@@ -3,9 +3,11 @@ function readCookies() {
     return cookieArr;
 }
 const jsonData = decodeURIComponent(readCookies()).split(';')
+console.log(jsonData)
 for(var i = 0; i < jsonData.length; i++){
     jsonData[i] = jsonData[i].split('=')[1].trim();
 }
 jsonData[0] = JSON.parse(jsonData[0])
+console.log(jsonData)
 jsonData = jsonData[0];
 module.exports = jsonData;
